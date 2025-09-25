29f38c6a63ad15983955ff981248a4a899dc621a
ivan ahumada <khabron@gmail.com>
2025-09-25 16:12:11 +0000

    usar este logo

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -6,12 +6,11 @@ export function Header() {
       <div className="container flex h-16 max-w-7xl items-center">
         <div className="mr-4 flex items-center">
           <Image
-            src="https://picsum.photos/seed/logo/40/40"
-            width={40}
-            height={40}
-            alt="Logo"
-            className="rounded-full"
-            data-ai-hint="logo"
+            src="https://storage.googleapis.com/studiopanda-assets/wwf-logo-scratch.png"
+            width={48}
+            height={48}
+            alt="Logo WWF"
+            className="dark:invert"
           />
           <h1 className="ml-3 font-headline text-2xl font-bold tracking-tight">
             WWF Cronología 2000
